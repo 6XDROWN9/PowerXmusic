652d769b89a1d69d3bc0cd291cefff75f4fe8536
xdrown <40439836+xdrown@users.noreply.github.com>
2018-07-05 12:59:20 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-exports.TOKEN = 'NDY0MzU3Nzg0MjQ3NTk5MTA0.Dh-B0w.SB0y8Fpkel1T3fTWeRgX7elbm5c';
+exports.TOKEN = 'NDY0MzU3Nzg0MjQ3NTk5MTA0.Dh-Lcg.aE4s-0pNXH_d3V4EjfltG5Ub_Go';
 
 exports.PREFIX = '!>';
 
